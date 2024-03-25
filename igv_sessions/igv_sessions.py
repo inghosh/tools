845997350f2215bcf2cb0226ee2ef81f3861bb37
ighosh <indro@ansabio.com>
2024-03-25 13:44:59 -0700

    igv_session template

---
+++ igv_sessions/igv_sessions.py
@@ -0,0 +1,26 @@
+"""
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<Session genome="referencefile_ref.fasta" locus="referencefile:0-244" version="8">
+    <Resources>
+        <Resource path="aligned_reads.bam" type="bam"/>
+    </Resources>
+    <Panel height="7086" name="Panel1682029927528" width="1884">
+        <Track attributeKey="aligned_reads.bam Coverage" autoScale="true" clazz="org.broad.igv.sam.CoverageTrack" fontSize="10" id="aligned_reads.bam_coverage" name="aligned_reads.bam Coverage" snpThreshold="0.2" visible="true">
+            <DataRange baseline="0.0" drawBaseline="true" flipAxis="false" maximum="23447.0" minimum="0.0" type="LINEAR"/>
+        </Track>
+        <Track attributeKey="aligned_reads.bam" clazz="org.broad.igv.sam.AlignmentTrack" color="185,185,185" displayMode="EXPANDED" experimentType="OTHER" fontSize="10" id="aligned_reads.bam" name="aligned_reads.bam" visible="true">
+            <RenderOptions/>
+        </Track>
+    </Panel>
+    <Panel height="150" name="FeaturePanel" width="1884">
+        <Track attributeKey="Reference sequence" clazz="org.broad.igv.track.SequenceTrack" fontSize="10" id="Reference sequence" name="Reference sequence" sequenceTranslationStrandValue="POSITIVE" shouldShowTranslation="false" visible="true"/>
+    </Panel>
+    <PanelLayout dividerFractions="0.009523809523809525,0.8359788359788359"/>
+    <HiddenAttributes>
+        <Attribute name="DATA FILE"/>
+        <Attribute name="DATA TYPE"/>
+        <Attribute name="NAME"/>
+    </HiddenAttributes>
+</Session>
+"""
+
